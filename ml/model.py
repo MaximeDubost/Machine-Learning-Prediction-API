5c67532735bdd0c24e28e8e4f630b5794d930b9d
MaximeDubost <maxime.dubost.75@gmail.com>
2021-01-05 16:54:16 +0100

    Model Building

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,16 @@
-from .utils.utils import DataHandler, FeatureRecipe, FeatureExtractor
+from utils.utils import DataHandler, FeatureRecipe, FeatureExtractor
 
 def DataManager(d:DataHandler=None, fr: FeatureRecipe=None, fe:FeatureExtractor=None):
     """
         Fonction qui lie les 3 premières classes de la pipeline et qui return FeatureExtractor.split(0.1)
     """
-    pass
+
+    data_handler = DataHandler()
+    df = data_handler.get_process_data()
+
+    feature_recipe = FeatureRecipe(df)
+    feature_recipe.prepare_data(0.3)
+
+    feature_extractor = FeatureExtractor(data_handler.df, list(['local_price', 'latitude', 'longitude', 'pricing_weekly_factor', 'pricing_monthly_factor', 'beds', 'bedrooms', 'bathrooms']))
+    feature_extractor.extract()
+    return feature_extractor.split_data(0.3, 42, 'local_price')
--- ml/utils/utils.py
+++ ml/utils/utils.py
@@ -2,41 +2,43 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import warnings, sys, pickle
-import datetime as dt
-warnings.simplefilter(action='ignore', category=FutureWarning)
+import joblib
 
 class DataHandler:
+    df1 = None
+    df2 = None
+    df = None
 
-    @classmethod
+    def __init__(self):
+        """
+        FeatureRecipe's Constructor
+        """
+
     def get_data(self):
         """ 
         Get data from GCS Bucket 
         """
         print('[1/2] Getting data... ', end='')
-        df1 = pd.read_csv('https://storage.googleapis.com/h3-data/listings_final.csv', sep=';')
-        df2 = pd.read_csv('https://storage.googleapis.com/h3-data/price_availability.csv', sep=';')
+        self.df1 = pd.read_csv('https://storage.googleapis.com/h3-data/listings_final.csv', sep=';')
+        self.df2 = pd.read_csv('https://storage.googleapis.com/h3-data/price_availability.csv', sep=';')
         print('Done.')
-        return [df1, df2]
     
-    @classmethod
     def get_group_data(self, data):
         """ 
         Merge both dataframes' data 
         """
         print('[2/2] Merging data... ', end='')
-        result = pd.merge(data[0], data[1].groupby('listing_id').local_price.mean('local_price'), how='inner', on='listing_id')
-        print('Done.')
-        return result
+        self.df = pd.merge(self.df1, self.df2.groupby('listing_id').local_price.mean('local_price'), how='inner', on='listing_id')
+        print('Done.\n')
+        return self.df
     
-    @classmethod
     def get_process_data(self):
         """ 
         Get & Merge data 
         """
         print("===| DataHandler |=== \n")
-        result = self.get_group_data(self.get_data())
-        return result
+        self.df = self.get_group_data(self.get_data())
+        return self.df
 
 
 class FeatureRecipe:
@@ -146,20 +148,20 @@ class FeatureRecipe:
         print("\n• Useless dropped columns (" + str(len(self.useless_columns)) + " column(s)) : " + str(self.useless_columns))
         print("\n• Duplicated dropped columns (" + str(len(self.duplicated_columns)) + " column(s)) : " + str(self.duplicated_columns))
         print("\n• Nan dropped columns by thresold (" + str(len(self.thresholded_columns)) + " column(s) for a thresold of " + str(threshold) + ") : " + str(self.thresholded_columns))
-        print("\n• Deal DateTime : Not Implemented")
+        print("\n• Deal DateTime : Not Implemented\n")
     
 
 class FeatureExtractor:
 
-    def __init__(self, data: pd.DataFrame, unused_columns:list):
+    def __init__(self, data: pd.DataFrame, feature_list:list):
         self.x_train, self.x_test, self.y_train, self.y_test = None, None, None, None
         self.data = data
-        self.unused_columns = unused_columns
+        self.feature_list = feature_list
         
     def drop_unusedf(self):
         print('[1/2] Dropping unused columns... ', end='')
         for column in self.data.columns:
-            if column in self.unused_columns:
+            if column not in self.feature_list:
                 self.data.drop(columns=column, inplace=True)
         print('Done.')
     
@@ -167,12 +169,11 @@ class FeatureExtractor:
         print('[2/2] Splitting data... ', end='')
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.data.loc[:, self.data.columns != target], self.data[target], test_size=test_size, random_state=random_state)
         print('Done.')
+        return self.x_train, self.x_test, self.y_train, self.y_test
         
     def extract(self):
         print("===| FeatureExtractor |=== \n")
         self.drop_unusedf()
-        self.split_data(0.3, 42, 'local_price')
-        return self.x_train, self.x_test, self.y_train, self.y_test
 
 
 class ModelBuilder:
@@ -185,7 +186,7 @@ class ModelBuilder:
         self.reg = LinearRegression()
     
     def __repr__(self):
-        pass
+        return f"ModelBuilder(path: {self.path}, save: {self.save}, reg: {self.reg})"
     
     def predict_test(self, X) -> np.ndarray:
         return self.reg.predict(X)
@@ -193,18 +194,19 @@ class ModelBuilder:
     def predict_from_dump(self, X) -> np.ndarray:
         pass
     
-    def save_model(self, path:str):
+    def save_model(self):
         # with the format : 'model_{}_{}'.format(date)
-        res = pickle.dumps(self.reg)
-        pickle.dump(res, "{}/model_{}.joblib".format(self.path, dt.datetime.now()))
+        joblib.dump(self.reg, "{}/model.joblib".format(self.path))
     
     def print_accuracy(self, X_test, y_test):
-        print("Accuracy : {}%".format(self.reg.score(X_test, y_test) * 100))
+        print("\n• Accuracy : {}%".format(self.reg.score(X_test, y_test) * 100))
     
     def load_model(self):
         try:
             # load model
-            return pickle.load("{}/model_{}.joblib".format(self.path, dt.datetime.now()))
+            return joblib.load("{}/model.joblib".format(self.path))
         except:
             print("File not found")
 
+    def train(self, X, y):
+        self.reg.fit(X, y)
